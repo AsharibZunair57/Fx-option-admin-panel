8bc0a1aeb4a555be4cc1b4e16acb1d60a72fa171
khanzadaimran <58213310+khanzadaimran@users.noreply.github.com>
2022-05-11 15:49:40 +0500

    table

--- src/Pages/ManageUsers/ActiveUsers.js
+++ src/Pages/ManageUsers/ActiveUsers.js
@@ -1,7 +1,90 @@
 import React from "react";
 
-const ActiveUsers = () => {
-  return <div>ActiveUsers</div>;
-};
+import { Table,  Space } from 'antd';
 
-export default ActiveUsers;
+const columns = [
+  {
+    title: 'User',
+    dataIndex: 'User',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Username',
+    dataIndex: 'Username',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  
+  {
+    title: 'Email',
+    dataIndex: 'Emailaddress',
+    key: 'email',
+  },
+  {
+    title: 'Phone',
+    dataIndex: 'phone',
+    key: 'phone',
+  },
+  {
+    title: 'Joined At',
+    dataIndex: 'JoinedAt',
+    key: 'phone',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+  
+  
+];
+
+const data = [
+  {
+    
+    key: '1',
+    User:'imran',
+    Username: 'John Brown',
+    Emailaddress:'imran@gmail.com',
+    phone:'025245245',
+    JoinedAt:'654',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+   
+  },
+  {
+    key: '2',
+    User:'imran',
+    Username: 'John Brown',
+    Emailaddress:'imran@gmail.com',
+    phone:'025245245',
+    JoinedAt:'654',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+   
+   
+  },
+  {
+    key: '3',
+    User:'imran',
+    Username: 'John Brown',
+    Emailaddress:'imran@gmail.com',
+    phone:'025245245',
+    JoinedAt:'654',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+   
+
+  },
+];
+
+export default () => <Table columns={columns} dataSource={data} />;
+
+
+//export default ActiveUsers;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
+// const dotenv = require("dotenv")
+// const mongoose = require('mongoose');
+
+// dotenv.config();
+
+// mongoose.connect(process.env.MONGO_URL,
+//   { useNewUrlParser:true,useUnifiedTopologay:true},
+//   ()=>{
+//   console.log("connected to mongoDB")
+// });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
